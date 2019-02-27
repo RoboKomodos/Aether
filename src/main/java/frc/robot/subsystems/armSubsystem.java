027049cca2b84d92154cb99934ba329072535da8
Jacob Malloy <sjakem@att.net>
2019-02-27 10:34:09 -0800

    add pid to the arm

--- src/main/java/frc/robot/subsystems/armSubsystem.java
+++ src/main/java/frc/robot/subsystems/armSubsystem.java
@@ -11,29 +11,40 @@ import java.io.Console;
 
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 import frc.robot.commands.moveArm;
 
 
 public class armSubsystem extends PIDSubsystem {
   CANSparkMax armMotor;
+  CANPIDController m_pidController;
+  private Double setPoint = 15.0;
   public armSubsystem() {
     super("arm", 0,0,0);
     armMotor = new CANSparkMax(RobotMap.armMotor, MotorType.kBrushless);
-
+    m_pidController = armMotor.getPIDController();
+    m_pidController.setP(0);
+    m_pidController.setI(0);
+    m_pidController.setD(0);
+    m_pidController.setOutputRange(-.25, .25);
+    SmartDashboard.putNumber("ARM P",m_pidController.getP());
   }
 
   public void set(double speed)
   {
-    if(Math.abs(speed) <.1)
+    /*if(Math.abs(speed) <.1)
     {
       speed = -.1;
     }
     armMotor.set(speed);
-    System.out.println(armMotor.getEncoder().getPosition());
+    System.out.println(armMotor.getEncoder().getPosition());*/
+    m_pidController.setP(SmartDashboard.getNumber("ARM P",m_pidController.getP()));
+    m_pidController.setReference(setPoint, ControlType.kPosition);
   }
 
   @Override
